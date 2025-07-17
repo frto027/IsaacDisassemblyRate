0b744be0c80af407320d301ab94fef8c715afec6
frto027 <q602706150@gmail.com>
2025-07-17 23:37:38 +0800

    add chest gen

---
+++ chest_gen.c
@@ -0,0 +1,73 @@
+char some_spawn_5_pre_update_variant_and_subtype(unsigned int seed,
+                                                 int *pVariant, int *pSubtype,
+                                                 int advanceRNG,
+                                                 int something_about_keeper) {
+
+  //.................
+  switch (*pVariant) {
+  case 50:
+    if (*pSubtype != 0) {
+      if (*pVariant == 52 && HasTrinket(151)) {
+        *pVariant = 54;
+      }
+    } else {
+      if (0 == RandInt(0x28u)) {
+        *pVariant = 54;
+      } else if (0 == RandInt(0x14u)) {
+        *pVariant = 360;
+      } else if (0 == RandInt(0x32u)) {
+        *pVariant = 51;
+      } else if (0 == RandInt(0x50u)) {
+        *pVariant = 52;
+      } else if (0 == RandInt(0x64u)) {
+        *pVariant = 69;
+        *pSubtype = 1;
+        if (0 == RandInt(0x64u) && (AchievementUnlocked[604])) {
+          *pSubtype = 2;
+        }
+        break;
+      } else if (!RandInt(0x50u) && AchievementUnlocked[609]) {
+        *pVariant = 56;
+      } else {
+        if (!RandInt(0x28u)) {
+          if (Room_GetRoomShape() != 2 && Room_GetRoomShape() != 3 &&
+              Room_GetRoomShape() != 7 && Room_GetRoomShape() != 5 &&
+              AchievementUnlocked[611]) {
+            *pVariant = 58;
+          }
+        }
+      }
+
+      if (!RandInt(1600u)) {
+        *pVariant = 53;
+      }
+
+      if (HasTrinket(61)) {
+        *pVariant = 360;
+      } else if (*pVariant == 52 && HasTrinket(151)) {
+        *pVariant = 54;
+      }
+      if (HasTrinket(159) && !HasTrinket(61) && *pVariant != 57) {
+        *pVariant = 60;
+      } else {
+        if (*pVariant == 60 && RandInt(0x78u) == 0 &&
+            AchievementUnlocked[601]) {
+          *pVariant = 57;
+        }
+      }
+
+      if (*pVariant == 57) {
+        *pSubtype = RandInt(4u) + 1 + RandInt(4u);
+      } else {
+        if (*pVariant == 53) {
+          *pSubtype = 2;
+        } else {
+          *pSubtype = 1;
+        }
+      }
+
+      break;
+    }
+  }
+  ///////////////..........
+}
